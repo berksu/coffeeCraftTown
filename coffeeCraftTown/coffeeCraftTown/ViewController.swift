d84ae7938698c2942343c67f0195d5e1353b7a7e
Berksu <berksukismet@gmail.com>
2017-10-08 17:46:39 +0300

    ilk commit

---
+++ coffeeCraftTown/coffeeCraftTown/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  coffeeCraftTown
+//
+//  Created by Berksu on 08/10/2017.
+//  Copyright © 2017 Berksu Kısmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //testNumber 01
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
