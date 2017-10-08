f24dbe32a073e1f19e9dc20316b67995bff78016
Kutan <cingisiz.akutan@gmail.com>
2017-10-08 23:24:38 +0300

    login fields

---
+++ coffeeCraftTown/coffeeCraftTown/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  coffeeCraftTown
+//
+//  Created by Kutan on 08/10/2017.
+//  Copyright © 2017 Berksu Kısmet. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginTextField(_ sender: Any) {
+    }
+    
+    @IBAction func passwordTextField(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
